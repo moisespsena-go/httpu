2b4bb8504d37ed62eefc20b042f42ba9b4c9e156
Moises P. Sena <moisespsena@gmail.com>
2019-03-27 10:03:46 -0300

    Fix listener gracefull shutdown

--- listener.go
+++ listener.go
@@ -2,6 +2,7 @@ package httpu
 
 import (
 	"context"
+	"io"
 	"net"
 	"net/http"
 	"sync"
@@ -36,6 +37,7 @@ type Listener struct {
 	Log         *logging.Logger
 	stop        bool
 	connections map[net.Conn]interface{}
+	connWg      sync.WaitGroup
 	mu          sync.RWMutex
 }
 
@@ -79,19 +81,55 @@ func (l *Listener) Start(done func()) (stop task.Stoper, err error) {
 	return l, nil
 }
 
-func (l *Listener) Shutdown(ctx context.Context) error {
+func (l *Listener) Shutdown(ctx context.Context) (err error) {
+	l.mu.Lock()
+	if l.stop {
+		l.mu.Unlock()
+		return
+	}
 	l.stop = true
-	return l.Server.Shutdown(ctx)
+	l.mu.Unlock()
+	return l.shutdown(ctx)
+}
+
+func (l *Listener) shutdown(ctx context.Context) (err error) {
+	l.mu.Lock()
+	l.stop = true
+	l.mu.Unlock()
+
+	finished := make(chan struct{}, 1)
+	go func() {
+		l.connWg.Wait()
+		finished <- struct{}{}
+	}()
+
+	defer l.Close()
+
+	select {
+	case <-ctx.Done():
+		for c := range l.connections {
+			c.Close()
+		}
+		return ctx.Err()
+	case <-finished:
+		return
+	}
+
+	return
 }
 
 func (l *Listener) ShutdownLog(ctx context.Context) (err error) {
-	if err = l.Shutdown(ctx); err != nil {
-		if err == context.DeadlineExceeded {
-			for con := range l.connections {
-				con.Close()
-			}
+	l.mu.Lock()
+	if l.stop {
+		l.mu.Unlock()
+		return
+	}
+	l.mu.Unlock()
+
+	if err = l.shutdown(ctx); err != nil {
+		if err != context.DeadlineExceeded {
+			l.Log.Errorf("Listener shutdown failed: %v", err)
 		}
-		l.Log.Errorf("Listener shutdown failed: %v", err)
 	} else {
 		l.Log.Info("Listener gracefully stopped")
 	}
@@ -102,7 +140,6 @@ func (l *Listener) Stop() {
 	if l.stop {
 		return
 	}
-
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	go l.ShutdownLog(ctx)
 }
@@ -110,16 +147,23 @@ func (l *Listener) Stop() {
 func (l *Listener) Accept() (con net.Conn, err error) {
 	con, err = l.Listener.Accept()
 	if err == nil {
+		l.mu.Lock()
+		defer l.mu.Unlock()
+		if l.stop || !l.running {
+			return nil, io.EOF
+		}
 		con = &connection{con, func() {
 			l.mu.Lock()
 			defer l.mu.Unlock()
-			delete(l.connections, con)
+			if _, ok := l.connections[con]; ok {
+				delete(l.connections, con)
+				l.connWg.Done()
+			}
 		}}
-		l.mu.Lock()
-		defer l.mu.Unlock()
 		if l.connections == nil {
 			l.connections = map[net.Conn]interface{}{}
 		}
+		l.connWg.Add(1)
 		l.connections[con] = nil
 	}
 	return
@@ -135,6 +179,18 @@ func (l *Listener) ListenAndServe() error {
 		return l.Server.ServeTLS(tcpKeepAliveListener{l}, l.Tls.CertFile, l.Tls.KeyFile)
 	}
 	return l.Server.Serve(l)
+}
+
+func (l *Listener) Close() (err error) {
+	l.mu.Lock()
+	defer func() {
+		l.running = false
+		l.mu.Unlock()
+	}()
+	if !l.running {
+		return
+	}
+	return l.Listener.Close()
 }
 
 type connection struct {
